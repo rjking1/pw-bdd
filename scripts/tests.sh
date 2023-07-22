ae74a7f149518491d49eb5305b68989c3cbf211d
Richard King <32266266+rjking1@users.noreply.github.com>
2023-07-22 11:47:05 +1000

    add tests script

---
+++ scripts/tests.sh
@@ -0,0 +1,7 @@
+# test cases
+npx bddgen  && npx playwright test
+npx bddgen " " && npx playwright test
+npx bddgen "@pw and @2002 and not  @2002.1" && npx playwright test
+npx bddgen "@pw and @2002 and @focus" && npx playwright test
+npx bddgen "@emergency and @4003" && npx playwright test
+npx bddgen "@emergency and not @4003" && npx playwright test
