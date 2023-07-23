41da66b88bc6e5508dd1965ac38b22e2531e5094
Richard King <32266266+rjking1@users.noreply.github.com>
2023-07-23 14:50:59 +1000

    Merge branch 'test-branch'

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,7 +1,8 @@
 # test cases
-npx bddgen  && npx playwright test
 npx bddgen " " && npx playwright test
 npx bddgen "@pw and @2002 and not  @2002.1" && npx playwright test
 npx bddgen "@pw and @2002 and @focus" && npx playwright test
 npx bddgen "@emergency and @4003" && npx playwright test
 npx bddgen "@emergency and not @4003" && npx playwright test
+# leave all tests built
+npx bddgen && npx playwright test
